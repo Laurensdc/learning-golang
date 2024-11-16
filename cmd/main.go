141fdef9df9d2d4ea9c6aaf04a90564d021b5c44
Laurens <laurens.decock@inthepocket.com>
2024-11-16 20:32:01 +0100

    ✨ DecodeInstructions now takes slice of bytes

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ func main() {
 		isDebug = true
 	}
 
-	bytes, err := os.ReadFile("../binaries/listing_0037_single_register_mov")
+	bytes, err := os.ReadFile("../binaries/listing_0038_many_register_mov")
 
 	if err != nil {
 		fmt.Printf("Failed to read file: %v\n", err)
@@ -26,77 +26,77 @@ func main() {
 		os.Exit(1)
 	}
 
-	var byteArr [2]byte
-	copy(byteArr[:], bytes[0:2])
-	instruction := decodeInstruction(byteArr)
+	instruction := DecodeInstructions(bytes)
 
 	fmt.Println(instruction)
-
 }
 
-func decodeInstruction(bytes [2]byte) string {
-
-	var byte1 byte = bytes[0]
-	var byte2 byte = bytes[1]
-
-	if isDebug {
-		fmt.Printf("Reading bytes %b %b\n", byte1, byte2)
-	}
-
-	// Decode it!
-	var opcode = byte1 & 0b1111_1100
-	var d = byte1 & 0b0000_0010 // 0: source is in reg field, 1: dest is in reg field
-	var w = byte1 & 0b0000_0001
-	var mod = byte2 & 0b1100_0000
-	var reg = byte2 & 0b0011_1000 >> 3 // name of register
-	var rm = byte2 & 0b0000_0111       // also name of register, or maybe name of memory Register/Memory R/M
-
-	if isDebug {
-		fmt.Printf("all the stuff: opcode %b d %b w %b mod %b reg %b rm %b\n", opcode, d, w, mod, reg, rm)
-	}
-
+func DecodeInstructions(bytes []byte) string {
 	decodedInstruction := ""
 
-	if opcode == 0b1000_1000 { // mov
-		decodedInstruction += "mov "
-		// d is 0 so
-		// source is in reg field
+	for i := 0; i < len(bytes)-1; i += 2 {
+		var byte1 byte = bytes[0]
+		var byte2 byte = bytes[1]
 
-		// when we do mov ax, bx
-		// it means   mov dest, source
-		// like						ax = bx
+		if isDebug {
+			fmt.Printf("Reading bytes %b %b\n", byte1, byte2)
+		}
 
-		// so now we know that source is in reg field
-		// so bx is in reg field
-		// that means that the dest is in the rm field
+		var opcode = byte1 & 0b1111_1100 >> 2 // first 6 bytes is opcode
+		var d = byte1 & 0b0000_0010 >> 1      // 0: source is in reg field, 1: dest is in reg field
+		var w = byte1 & 0b0000_0001
 
-		// now I need to get dest first
+		var mod = byte2 & 0b1100_0000 >> 6
+		var reg = byte2 & 0b0011_1000 >> 3 // name of register
+		var rm = byte2 & 0b0000_0111       // also name of register, or maybe name of memory Register/Memory R/M
 
-		if d == 0 { // source is in reg field, dest is in r/m field
-			if w == 1 {
-				// Map for when W == 1
-				registerMapW1 := map[byte]string{
-					0b000: "ax",
-					0b001: "cx",
-					0b010: "dx",
-					0b011: "bx",
-					0b100: "sp",
-					0b101: "bp",
-					0b110: "si",
-					0b111: "di",
+		if isDebug {
+			fmt.Printf("all the stuff: opcode %b d %b w %b mod %b reg %b rm %b\n", opcode, d, w, mod, reg, rm)
+		}
+
+		if opcode == 0b1000_10 { // mov
+			decodedInstruction += "mov "
+			// d is 0 so
+			// source is in reg field
+
+			// when we do mov ax, bx
+			// it means   mov dest, source
+			// like						ax = bx
+
+			// so now we know that source is in reg field
+			// so bx is in reg field
+			// that means that the dest is in the rm field
+
+			// now I need to get dest first
+
+			if d == 0 { // source is in reg field, dest is in r/m field
+				if w == 1 {
+					// Map for when W == 1
+					registerMapW1 := map[byte]string{
+						0b000: "ax",
+						0b001: "cx",
+						0b010: "dx",
+						0b011: "bx",
+						0b100: "sp",
+						0b101: "bp",
+						0b110: "si",
+						0b111: "di",
+					}
+
+					destRegister := registerMapW1[rm]
+					decodedInstruction += destRegister + ", "
+
+					sourceRegister := registerMapW1[reg]
+					decodedInstruction += sourceRegister
+				} else if w == 0 {
+					// TODO: Create map for w == 0
 				}
 
-				destRegister := registerMapW1[rm]
-				decodedInstruction += destRegister + ", "
-
-				sourceRegister := registerMapW1[reg]
-				decodedInstruction += sourceRegister
-			} else if w == 0 {
-				// TODO: Create map for w == 0
 			}
 
 		}
 
+		decodedInstruction += "\n"
 	}
 
 	if isDebug {
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,14 +2,26 @@ package main
 
 import "testing"
 
-func TestDecodeInstruction(t *testing.T) {
+func TestDecodeInstruction_SingleRegister(t *testing.T) {
 	var byte1 byte = 0b10001001
 	var byte2 byte = 0b11011001
 
-	output := DecodeInstruction([2]byte{byte1, byte2})
-	expected := "mov cx, bx"
+	output := DecodeInstructions([]byte{byte1, byte2})
+	expected := "mov cx, bx\n"
 
 	if output != expected {
-		t.Errorf("Expected %v, got %v", expected, output)
+		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
+	}
+}
+
+func TestDecodeInstruction_MultipleRegisters(t *testing.T) {
+	var byte1 byte = 0b10001001
+	var byte2 byte = 0b11011001
+
+	output := DecodeInstructions([]byte{byte1, byte2, byte1, byte2})
+	expected := "mov cx, bx\nmov cx, bx\n"
+
+	if output != expected {
+		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
 	}
 }
