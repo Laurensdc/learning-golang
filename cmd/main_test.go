783b8cb7e0e3c72005dcd5b4923f925eb90eff9c
Laurens <laurens.decock@inthepocket.com>
2024-11-16 20:40:08 +0100

    🧪 Add failing test for complete mov decoding

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -15,11 +15,21 @@ func TestDecodeInstruction_SingleRegister(t *testing.T) {
 }
 
 func TestDecodeInstruction_MultipleRegisters(t *testing.T) {
-	var byte1 byte = 0b10001001
-	var byte2 byte = 0b11011001
-
-	output := DecodeInstructions([]byte{byte1, byte2, byte1, byte2})
-	expected := "mov cx, bx\nmov cx, bx\n"
+	bytes := []byte{137, 217, 136, 229, 137, 218, 137, 222, 137, 251, 136, 200, 136, 237, 137, 19, 5, 137, 243, 137, 252, 137, 197}
+	output := DecodeInstructions(bytes)
+	expected :=
+		`mov cx, bx
+mov ch, ah
+mov dx, bx
+mov si, bx
+mov bx, di
+mov al, cl
+mov ch, ch
+mov bx, ax
+mov bx, si
+mov sp, di
+mov bp, ax
+`
 
 	if output != expected {
 		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
